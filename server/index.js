ec79500815dfdd2c21e63b828953f84cea1c03e4
Harshit Mourya <mouryaharshit03@gmail.com>
2025-01-15 19:44:25 +0530

    deploying project

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ const cors = require("cors");
 const corsOptions = {
   origin: ["http://localhost:5173", "https://shoe-spot.onrender.com"],
   methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
 };
 app.use(cors(corsOptions));
 
